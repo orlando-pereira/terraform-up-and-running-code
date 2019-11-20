e4e0d2e57d4f62c218c034de9c6929f984a99331
orpere <orlando.capoeiraraiz@gmail.com>
2019-11-20 16:54:40 +0100

    state

--- code/terraform/04-terraform-module/multi-repo-example/live/prod/services/webserver-cluster/variables.tf
+++ code/terraform/04-terraform-module/multi-repo-example/live/prod/services/webserver-cluster/variables.tf
@@ -23,3 +23,15 @@ variable "cluster_name" {
   type        = string
   default     = "webservers-prod"
 }
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "orlando-locks"
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "/prod/data-stores/mysql/terraform.tfstate"
+}
--- code/terraform/04-terraform-module/multi-repo-example/live/stage/services/webserver-cluster/variables.tf
+++ code/terraform/04-terraform-module/multi-repo-example/live/stage/services/webserver-cluster/variables.tf
@@ -23,3 +23,15 @@ variable "cluster_name" {
   type        = string
   default     = "webservers-stage"
 }
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "orlando-locks"
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+  default     = "/stage/data-stores/mysql/terraform.tfstate"
+}
