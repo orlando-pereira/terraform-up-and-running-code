f58b7c4de8bcb856d34d8f516e08546964fd5993
orpere <orlando.capoeiraraiz@gmail.com>
2019-11-20 16:34:32 +0100

    my backend

--- code/terraform/03-terraform-state/file-layout-example/global/s3/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/global/s3/main.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_version = ">= 0.12, < 0.13"
-}
-
 provider "aws" {
   region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
